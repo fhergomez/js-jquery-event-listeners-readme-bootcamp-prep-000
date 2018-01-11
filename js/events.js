54d42b0bad561c438115ea02ef66611301bd47c7
Fernando Gomez <fhergomez37@gmail.com>
2018-01-11 03:48:05 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -2,13 +2,25 @@
 function getIt() {
   $('p').on('click', function(){
     alert('hey!');
-  })
+  });
 }
 
 function frameIt() {
   $('img').on('load', function () {
     $('img').addClass('tasty');
-  })
+  });
+}
+
+function pressIt() {
+  $('input').on('keydown', function() {
+    if(key === 71) {
+      alert('You\'ve preseed "G"');
+    }
+  });
+}
+
+function submitIt() {
+
 }
 
 $(document).ready(function(){
